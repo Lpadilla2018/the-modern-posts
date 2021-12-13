c6223e69c96e6b443863cb299a46ba85a32335ea
Louie Padilla <lapadil3@asu.edu>
2021-12-13 09:50:28 -0700

    Add email to commement info

--- src/components/comment-info/comment-info.component.jsx
+++ src/components/comment-info/comment-info.component.jsx
@@ -1,9 +1,10 @@
 import './comment-info.component.styles.scss';
 
-const CommentInfo = ({ name, body }) => {
+const CommentInfo = ({ name, body, email }) => {
   return (
     <div className='comment-info'>
       <h4>{name}</h4>
+      <p>{email}</p>
       <p>{body}</p>
     </div>
   );
